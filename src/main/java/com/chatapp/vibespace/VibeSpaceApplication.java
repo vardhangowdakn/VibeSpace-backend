c2aca0cf71326a8129e7262496bc4dbb6cca430b
Vardhan Gowda K N <vardhangowdakn@gmail.com>
2025-09-30 19:03:36 +0530

    Initial FusionAI backend commit

---
+++ src/main/java/com/chatapp/vibespace/VibeSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.vibespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/vibespace/VibeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.vibespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
