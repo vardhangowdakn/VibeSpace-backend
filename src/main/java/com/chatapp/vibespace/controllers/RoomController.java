c08c0dad1473ad0fe983928ee1ac23ec6fc7ed25
Vardhan Gowda K N <vardhangowdakn@gmail.com>
2025-10-01 13:24:03 +0530

    creating RestAPIs for RoomController

--- src/main/java/com/chatapp/vibespace/controllers/RoomController.java
+++ src/main/java/com/chatapp/vibespace/controllers/RoomController.java
@@ -1,4 +1,66 @@
 package com.chatapp.vibespace.controllers;
 
+import com.chatapp.vibespace.entities.Message;
+import com.chatapp.vibespace.entities.Room;
+import com.chatapp.vibespace.repositories.RoomRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/rooms")
 public class RoomController {
+
+    private RoomRepository roomRepository;
+
+    public RoomController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createRoom(@RequestBody String roomId){
+        if(roomRepository.findByRoomId(roomId) != null){
+            return ResponseEntity.badRequest().body("Room already exists");
+        }
+
+        Room room = new Room();
+        room.setRoomId(roomId);
+        Room saveRoom = roomRepository.save(room);
+        return ResponseEntity.status(HttpStatus.CREATED).body(room);
+
+
+    }
+    @GetMapping("/{roomId}")
+    public ResponseEntity<?> joinRoom(
+            @PathVariable String roomId
+    ) {
+
+        Room room = roomRepository.findByRoomId(roomId);
+        if (room == null) {
+            return ResponseEntity.badRequest()
+                    .body("Room not found!!");
+        }
+        return ResponseEntity.ok(room);
+    }
+
+    @GetMapping("/{roomId}/messages")
+    public ResponseEntity<List<Message>> getMessages(
+            @PathVariable String roomId,
+            @RequestParam(value = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(value = "size", defaultValue = "20", required = false) int size
+    ) {
+        Room room = roomRepository.findByRoomId(roomId);
+        if (room == null) {
+            return ResponseEntity.badRequest().build()
+                    ;
+        }
+        List<Message> messages = room.getMessages();
+        int start = Math.max(0, messages.size() - (page + 1) * size);
+        int end = Math.min(messages.size(), start + size);
+        List<Message> paginatedMessages = messages.subList(start, end);
+        return ResponseEntity.ok(paginatedMessages);
+
+    }
 }
